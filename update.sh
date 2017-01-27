df54f7ecec7ece0a5d514a1f3539f405297a1847
rekire <git@reki.re>
2017-01-27 23:59:39 +0100

    Optimize the travis script

---
+++ update.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+echo Analyse the libraries...
+java -jar scanner/build/libs/scanner.jar PlayServices $ANDROID_HOME/extras/google/m2repository/com/google/android/gms
+java -jar scanner/build/libs/scanner.jar SupportLibrary $ANDROID_HOME/extras/android/m2repository/com/android/support
+echo Compare output with current stored version...
+mkdir tmp
+mv *.csv *.json tmp
+git checkout gh-pages -q
+rm *.csv *.json
+mv tmp/* .
+rmdir tmp
+echo Changes found:
+git ls-files --other --directory --exclude-standard --no-empty-directory
