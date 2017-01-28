9de47db22496cca3c096cc6d5f1bb07dad0a84eb
rekire <git@reki.re>
2017-01-28 16:06:04 +0100

    Changed the order of some commands

--- update.sh
+++ update.sh
@@ -7,6 +7,12 @@ echo Compare output with current stored version...
 git clone --depth=50 --branch=gh-pages https://github.com/rekire/DependencyLookup.git gh-pages
 rm gh-pages/*.csv gh-pages/*.json
 mv *.csv *.json gh-pages
+
+# prepare the key
+chmod 600 bot_id_rsa
+eval `ssh-agent -s`
+ssh-add bot_id_rsa
+
 cd gh-pages
 touch `date +Test-%H%M%S`
 if [ `git ls-files --other --directory --exclude-standard | wc -l` -eq 0 ]; then
@@ -19,10 +25,7 @@ git config user.name "Travis CI"
 git config user.email "bot@reki.re"
 git commit -m "Automatic documentation update"
 
-# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
-chmod 600 bot_id_rsa
-eval `ssh-agent -s`
-ssh-add bot_id_rsa
+git log
 
 # Now that we're all set up, we can push.
-git push origin gh-pages
+#git push origin gh-pages
